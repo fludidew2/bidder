fa900b985273da6c1ee29b0fffc33b42263df393
Rami <rami@sharkeystudio.com>
2024-09-26 11:35:26 -0500

    changed the game

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,23 +2,19 @@
   before_action :authenticate_user!
 
   def index
-    @requests = if current_user.buyer?
-                  # Show the user's own requests if they are a buyer
-                  Request.where(user_id: current_user.id).order(created_at: :desc)
+    @accepted_requests_count = Request.where(status: 'accepted', winning_bid_user_id: current_user.id).count
+    @completed_requests_count = Request.where(status: 'completed', winning_bid_user_id: current_user.id).count
+    @live_requests_count = Request.where(status: 'live').count
+    
+    @requests = case params[:status]
+                when 'accepted', 'completed'
+                  Request.where(status: params[:status], winning_bid_user_id: current_user.id)
+                         .where.not(id: DeclinedRequest.where(user_id: current_user.id).select(:request_id))
+                         .order(created_at: :desc)
                 else
-                  @accepted_requests_count = Request.where(status: 'accepted', winning_bid_user_id: current_user.id).count
-                  @completed_requests_count = Request.where(status: 'completed', winning_bid_user_id: current_user.id).count
-                  @live_requests_count = Request.where(status: 'live').count
-                  case params[:status]
-                  when 'accepted', 'completed'
-                    Request.where(status: params[:status], winning_bid_user_id: current_user.id)
-                           .where.not(id: DeclinedRequest.where(user_id: current_user.id).select(:request_id))
-                           .order(created_at: :desc)
-                  else
-                    Request.where(status: 'live')
-                           .where.not(id: DeclinedRequest.where(user_id: current_user.id).select(:request_id))
-                           .order(created_at: :desc)
-                  end
+                  Request.where(status: 'live')
+                         .where.not(id: DeclinedRequest.where(user_id: current_user.id).select(:request_id))
+                         .order(created_at: :desc)
                 end
   
     respond_to do |format|
